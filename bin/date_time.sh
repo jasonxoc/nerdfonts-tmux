5469f2b1a913ce80bc6deb7a5193284c7647aa90
Jason <jason.cox@voicemediagroup.com>
2019-07-20 18:49:01 -0700

    Added terminal column

--- bin/date_time.sh
+++ bin/date_time.sh
@@ -6,8 +6,15 @@
 # Rep: https://github.com/jasonxoc/nerdfonts-tmux
 #
 # Notes: This is made for me, uses macos commands
+#        I want to try to see how many cols the term has
+#        and if theres enough room, display the large date
+#        I think I need to execute `tput cols` inside of tmux
+#        and pass it in as an argument becuase these scripts run
+#        in shell level 3 rather than 2...
 #
 # # # # # # # # # #
+
+cars_per_line="$(tput cols)"
 
 date_time=" $(date '+%a %l:%M')"
 date_time_aux=" $(date '+%a %m/%d%l:%M')"
